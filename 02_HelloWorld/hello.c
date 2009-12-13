300333d8b86609c003661daa66ae60f28d68b455
Derek Dai <derekdai@qcomp.(none)>
2009-12-13 23:04:01 +0800

    Add unified constructor calling sequence to object model

--- 02_HelloWorld/hello.c
+++ 02_HelloWorld/hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 #define TRUE		(1)
 
@@ -20,10 +21,12 @@ typedef enum
 
 typedef enum
 {
-	KLASS_FLAG_ABSTRACT = 1 << 0,
+	KLASS_FLAG_ABSTRACT	= 1 << 0,
 } KlassFlag;
 
 typedef struct _Klass Klass;
+
+typedef void (* ObjectInitFunc)(void *);
 
 struct _Klass
 {
@@ -34,6 +37,8 @@ struct _Klass
 	KlassFlag flags;
 
 	KlassId parent_id;
+
+	ObjectInitFunc init;
 };
 
 typedef struct _Object
@@ -67,13 +72,18 @@ typedef struct _Line
 	int y2;
 } Line;
 
-void shape_init(Shape * shape, int x, int y)
+void shape_init_full(Shape * shape, int x, int y)
 {
 	shape->x = x;
 	shape->y = y;
 }
 
-void rectangle_init(
+void shape_init(Shape * shape)
+{
+	shape_init_full(shape, 0, 0);
+}
+
+void rectangle_init_full(
 		Rectangle * rectangle,
 		int x,
 		int y,
@@ -84,18 +94,22 @@ void rectangle_init(
 		return;
 	}
 
-	shape_init((Shape *) rectangle, x, y);
 	rectangle->height = height;
 	rectangle->width = width;
 }
 
-Rectangle * rectangle_new(int x, int y, int width, int height)
+void rectangle_init(Rectangle * rectangle)
 {
-	Rectangle * self = (Rectangle *) malloc(sizeof(Rectangle));
-	rectangle_init(self, x, y, width, height);
-
-	return self;
+	rectangle_init_full(rectangle, 0, 0, 0, 0);
 }
+
+//Rectangle * rectangle_new(int x, int y, int width, int height)
+//{
+//	Rectangle * self = (Rectangle *) malloc(sizeof(Rectangle));
+//	rectangle_init(self, x, y, width, height);
+//
+//	return self;
+//}
 
 void rectangle_draw(Rectangle * self)
 {
@@ -106,22 +120,26 @@ void rectangle_draw(Rectangle * self)
 			self->height);
 }
 
-void line_init(Line * line, int x, int y, int x2, int y2)
+void line_init_full(Line * line, int x, int y, int x2, int y2)
 {
 	if (!line)	return;
 
-	shape_init((Shape *)line, x, y);
 	line->x2 = x2;
 	line->y2 = y2;
 }
 
-Line * line_new(int x, int y, int x2, int y2)
+void line_init(Line * line)
 {
-	Line * self = (Line *) malloc(sizeof(Line));
-	line_init(self, x, y, x2, y2);
-
-	return self;
+	line_init_full(line, 0, 0, 0, 0);
 }
+
+//Line * line_new(int x, int y, int x2, int y2)
+//{
+//	Line * self = (Line *) malloc(sizeof(Line));
+//	line_init(self, x, y, x2, y2);
+//
+//	return self;
+//}
 
 void line_draw(Line * self)
 {
@@ -133,14 +151,30 @@ void line_draw(Line * self)
 	);
 }
 
+void point_init(Point * point)
+{
+}
+
 const Klass klasses[KLASS_ID_MAX] = {
 		{ KLASS_ID_INVALID },
-		{ KLASS_ID_OBJECT, sizeof(Object), KLASS_FLAG_ABSTRACT, KLASS_ID_INVALID },
-		{ KLASS_ID_SHAPE, sizeof(Shape), KLASS_FLAG_ABSTRACT, KLASS_ID_OBJECT },
-		{ KLASS_ID_RECTANGLE, sizeof(Rectangle), 0, KLASS_ID_SHAPE },
-		{ KLASS_ID_LINE, sizeof(Line), 0, KLASS_ID_SHAPE },
-		{ KLASS_ID_POINT, sizeof(Point), 0, KLASS_ID_SHAPE },
+		{ KLASS_ID_OBJECT, sizeof(Object), KLASS_FLAG_ABSTRACT, KLASS_ID_INVALID, NULL },
+		{ KLASS_ID_SHAPE, sizeof(Shape), KLASS_FLAG_ABSTRACT, KLASS_ID_OBJECT, (ObjectInitFunc) shape_init },
+		{ KLASS_ID_RECTANGLE, sizeof(Rectangle), 0, KLASS_ID_SHAPE, (ObjectInitFunc) rectangle_init},
+		{ KLASS_ID_LINE, sizeof(Line), 0, KLASS_ID_SHAPE, (ObjectInitFunc) line_init },
+		{ KLASS_ID_POINT, sizeof(Point), 0, KLASS_ID_SHAPE, (ObjectInitFunc) point_init },
 };
+
+void object_run_init(const Klass * klass, void * object)
+{
+	if(KLASS_ID_INVALID == klass->id) {
+		return;
+	}
+
+	object_run_init(& klasses[klass->parent_id], object);
+	if(klass->init) {
+		klass->init(object);
+	}
+}
 
 Object * object_new(KlassId id)
 {
@@ -159,6 +193,7 @@ Object * object_new(KlassId id)
 
 	self = (Object *) malloc(klass->size);
 	if(self) {
+		object_run_init(klass, self);
 		self->klass = klass;
 	}
 
@@ -187,19 +222,11 @@ bool object_is_a(Object * obj, KlassId id)
 
 int main()
 {
-//	char * test = "Hello World!!";
-//	printf("%s\n", test);
-
 	Rectangle * A;
 	Line * B;
 	Point * C;
 	Shape * s;
 	Object * o;
-
-	o = object_new(KLASS_ID_POINT);
-	if(object_is_a(o, KLASS_ID_SHAPE)) {
-		s = (Shape *) o;
-	}
 
 	//A = rectangle_new(10,20, 100, 50);
 	A = (Rectangle *) object_new(KLASS_ID_RECTANGLE);
@@ -209,5 +236,10 @@ int main()
 	B = (Line *) object_new(KLASS_ID_LINE);
 	line_draw(B);
 	
+	o = object_new(KLASS_ID_POINT);
+	if(object_is_a(o, KLASS_ID_SHAPE)) {
+		s = (Shape *) o;
+	}
+
 	return 0;
 }
